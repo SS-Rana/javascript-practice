917bde00bd52d2b3d2ff955a6c5a71573c87a95b
SS-Rana <146541878+SS-Rana@users.noreply.github.com>
2024-07-01 10:46:55 +0530

    commit

--- Counter/index.js
+++ Counter/index.js
@@ -1,15 +1,15 @@
-let countEl = document.getElementById("count-el")
-let saveCount = document.getElementById("save-el")
-let count = 0
-function increment (){
-    countEl.innerText = count
-    count ++
-    console.log(count.innerText)
+let countEl = document.getElementById("count-el");
+let saveCount = document.getElementById("save-el");
+let count = 0;
 
+function increment() {
+    count += 1;
+    countEl.innerText = count;
+    console.log(count);
 }
 
-function save(){
-    saveCount.textContent += count + " - "
-    count.innerText = 0
-    count = 0
+function save() {
+    saveCount.textContent += count + " - ";
+    count = 0;
+    countEl.innerText = count;
 }
